afdedbb1b472bc239e5867ad3ca57f2109c61848
Mauricio Brito <britomauricio2003@gmail.com>
2022-06-23 02:03:53 -0300

    Pagina de requisicoes aluno

--- public/scripts/aluno/requisicao/requisicoes.js
+++ public/scripts/aluno/requisicao/requisicoes.js
@@ -14,26 +14,26 @@ async function iniciarRequisicoesAluno(parametros) {
 async function mostrarRequisicoesAluno() {
     carregamento();
     const respReqAluno = await resultadoRequisicoesAluno();
-    console.log(respReqAluno);
-    // const respHistorico = await resultadoHistoricoIdQuestionario(parametrosJsonRelatorio["idQuestionario"]);
-    // if(respHistorico && respHistorico["status"]!=erroRequisicao) {
-    //     document.querySelector("div#principalRelatorio #conteudoRelatorio table tbody").innerHTML = "";
-    //     for(let x=0; x<respHistorico["resultados"].length; x++) {
-    //         const linhaTabela = `
-    //             <tr ${x % 2 == 0 ? '' : 'class="cinza"'}>
-    //                 <td>${respHistorico["resultados"].length - x}</td>
-    //                 <td>${formatarData(respHistorico["resultados"][x]["data"])}</td>
-    //                 <td>${respHistorico["resultados"][x]["hora"]}</td>
-    //                 <td>${respHistorico["resultados"][x]["qtdAcertos"]}</td>
-    //                 <td>${respHistorico["resultados"][x]["qtdErros"]}</td>
-    //                 <td>${arredondar(respHistorico["resultados"][x]["nota"])}</td>
-    //                 <td><button onclick="revisaoQuestionario(${respHistorico["resultados"][x]["idResponde"]})"><p>Revisao</p></button></td>
-    //             </tr>
-    //         `;
-    //         document.querySelector("div#principalRelatorio #conteudoRelatorio table tbody").innerHTML += linhaTabela;
-    //     }
-    // } else if (respHistorico && respHistorico["status"]==erroRequisicao && respHistorico["mensagem"] == "Nenhum registro encontrado!") {
-    //     document.querySelector("div#principalRelatorio #conteudoRelatorio").innerHTML = `<p>${respHistorico["mensagem"]}</p>`;
-    // }
+    if(respReqAluno && respReqAluno["status"]!=erroRequisicao && respReqAluno["resultados"].length > 0) {
+        respReqAluno["resultados"].sort(function(a,b) {
+            return (new Date(b["data"]).getTime() - new Date(a["data"]).getTime())
+        });
+        document.querySelector("div#principalRequisicaoAluno #conteudoRequisicaoAluno table tbody").innerHTML = "";
+        for(let x=0; x<respReqAluno["resultados"].length; x++) {
+            const linhaTabela = `
+                <tr ${x % 2 == 0 ? '' : 'class="cinza"'}>
+                    <td>${respReqAluno["resultados"][x]["tipo"]}</td>
+                    <td>${respReqAluno["resultados"][x]["nome"+respReqAluno["resultados"][x]["tipo"]]}</td>
+                    <td>${formatarData(respReqAluno["resultados"][x]["data"])}</td>
+                    <td>${respReqAluno["resultados"][x]["dataAvaliado"] != null ? formatarData(respReqAluno["resultados"][x]["dataAvaliado"]) : "-----"}</td>
+                    <td>${respReqAluno["resultados"][x]["mensagem"] != null ? respReqAluno["resultados"][x]["mensagem"] : "-----"}</td>
+                    <td class="status"><p class="statusReq" style="background-color: ${respReqAluno["resultados"][x]["status"] == null ? "#e9ab2c" : respReqAluno["resultados"][x]["status"] == "Recusado" ? "#ff5a5a" : "#37c537"}">${respReqAluno["resultados"][x]["status"] != null ? respReqAluno["resultados"][x]["status"] : "Pendente"}</p></td>
+                </tr>
+            `;
+            document.querySelector("div#principalRequisicaoAluno #conteudoRequisicaoAluno table tbody").innerHTML += linhaTabela;
+        }
+    } else if (respReqAluno && respReqAluno["status"]==erroRequisicao && respReqAluno["mensagem"] == "Nenhum registro encontrado!" || respReqAluno["resultados"].length <= 0) {
+        document.querySelector("div#principalRequisicaoAluno #conteudoRequisicaoAluno").innerHTML = `<p>Nenhum registro encontrado</p>`;
+    }
     pararCarregamento();
 }
